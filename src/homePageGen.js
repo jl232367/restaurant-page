8e5a6739f66c29fffcc7d3a93735b1fd37349668
Jacob Luallen <jluallen@fcc-jc.org>
2021-06-18 18:43:12 -0400

    not sure what more scripts I would want.  Need to build the function that will make the home page

---
+++ src/homePageGen.js
@@ -0,0 +1,13 @@
+
+
+
+/*
+<div class="banner">
+                <h1 id="headline">AWESOME!</h1>
+            </div>
+            <div id="markup">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis libero leo, maximus quis eleifend sed, vestibulum vitae lorem. Nam fermentum mollis sem, ut tristique augue lobortis et. Sed ac fringilla diam. Maecenas feugiat nisl et dapibus blandit. Donec ac lorem nec ex dapibus viverra. Pellentesque viverra nec ligula id pulvinar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget tempus eros. Etiam egestas lectus in lorem consectetur volutpat. Morbi lobortis vel risus quis lacinia. Nullam et arcu nec ligula posuere aliquam. Fusce et rutrum diam. Aliquam eleifend laoreet quam, id consequat nibh rutrum at. Fusce congue erat id dictum feugiat.
+                </p>
+            </div>
+*/
