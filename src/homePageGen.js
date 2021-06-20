5fcf59cc24f738885367b27c224833d4a6a16733
Jacob Luallen <jluallen@fcc-jc.org>
2021-06-20 17:16:44 -0400

    made function to generate homepage, need to add buttons on page to execute.

--- src/homePageGen.js
+++ src/homePageGen.js
@@ -1,4 +1,21 @@
+const homePageGen = () => {
+    const content = document.getElementById("content");
+    const banner = document.createElement("div");
+    const awesome = document.createElement("h1");
+    const markUp = document.createElement("div");
+    const markUpcontent = document.createElement("p");
+    content.appendChild(banner);
+    content.appendChild(markUp);
+    banner.appendChild(awesome);
+    markUp.appendChild(markUpcontent);
+    banner.setAttribute('class', "banner");
+    awesome.setAttribute('id', "headline");
+    markUp.setAttribute('id', "markup");
+    awesome.innerHTML = "AWESOME!";
+    markUpcontent.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis libero leo, maximus quis eleifend sed, vestibulum vitae lorem. Nam fermentum mollis sem, ut tristique augue lobortis et. Sed ac fringilla diam. Maecenas feugiat nisl et dapibus blandit. Donec ac lorem nec ex dapibus viverra. Pellentesque viverra nec ligula id pulvinar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam eget tempus eros. Etiam egestas lectus in lorem consectetur volutpat. Morbi lobortis vel risus quis lacinia. Nullam et arcu nec ligula posuere aliquam. Fusce et rutrum diam. Aliquam eleifend laoreet quam, id consequat nibh rutrum at. Fusce congue erat id dictum feugiat."
+}
 
+export {homePageGen}
 
 
 /*
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello! I executed, hope!")
+import { homePageGen } from "./homePageGen";
+
+const clearPage = () => {
+    const contentToClear = document.getElementById("content");
+    while (contentToClear.firstChild) {
+        contentToClear.removeChild(contentToClear.firstChild);
+    }
+}
+
+
+// console.log("Hello! I executed, hope!")
